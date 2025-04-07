863825b94c4482b4d06e95f563730b28ff17344a
viniciusbp7 <vinicius.b.pomilio@gmail.com>
2025-04-07 10:11:14 -0300

    exs 1 tentativa 1

--- exe1/main.c
+++ exe1/main.c
@@ -24,7 +24,24 @@ void adc_1_task(void *p) {
 
         // CÓDIGO AQUI
 
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
 
+void adc_2_task(void *p) {
+    adc_init();
+    adc_gpio_init(26);
+
+    // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
+    const float conversion_factor = 3.3f / (1 << 12);
+
+    uint16_t result;
+    while (1) {
+        adc_select_input(0); // Select ADC input 1 (GPIO26)
+        result = adc_read();
+        printf("voltage 2: %f V\n", result * conversion_factor);
+
+        // CÓDIGO AQUI
 
         vTaskDelay(pdMS_TO_TICKS(200));
     }
@@ -36,6 +53,7 @@ int main() {
     adc_init();
 
     xTaskCreate(adc_1_task, "LED_Task 1", 4095, NULL, 1, NULL);
+    xTaskCreate(adc_2_task, "LED_Task 2", 4095, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while (true) {
